1b4fd4537420f1724c2e623f97b7e310a387b8f6
CesarSM1 <cesarsmore@gmail.com>
2023-02-22 19:44:47 -0600

    Pruebas
    oa

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Compiladores-
-Código fuente del intérprete para la materia de compiladores, grupo 5CV3 (:
+Código fuente del intérprete para la materia de compiladores
+Grupo 5CV3 (:
+
+Equipo:
+
+1) Oviedo Rubalcava Samuel Barush.
+2) Saucedo Moreno César Enrique.
---
+++ compiladores/src/compiladores/Compiladores.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package compiladores;
+
+/**
+ *
+ * @author cesar
+ */
+public class Compiladores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i;
+        
+        
+    }
+    
+}
