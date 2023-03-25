315bd2a20ed5fd07284e924425e72bae468ecf8f
SamuelBarush <soviedor1700@alumno.ipn.mx>
2023-03-25 13:23:19 -0600

    Update Scanner.java

--- Interprete/Scanner.java
+++ Interprete/Scanner.java
@@ -41,6 +41,7 @@ public class Scanner {
         this.Codigo = Codigo;
     }
 
+    int retractar (int i){ i--; return i;}
     List < Token > ScanTokens()
     {
 
@@ -50,37 +51,46 @@ public class Scanner {
         char Caracter;   
         StringBuilder Palabra = new StringBuilder();
         String NuevoLexema = null;
+
+        if(Character.isLetter('y')) System.out.println("es una 'y'");
+
         for (int i = 0; i < Codigo.length(); i++)
         {
             Caracter = Codigo.charAt(i);
 
             switch (estado) {
                 case 9:
-                    System.out.println("Caso 9");
+                    //System.out.println("Caso 9");
                     if (Character.isDigit(Caracter)){ estado = 8; Palabra.append(Caracter); }
-                    else if(!Character.isDigit(Caracter)){ estado = 10; Palabra.append(Caracter); }                    
+                    else if(Character.isLetter(Caracter)){ estado = 10; Palabra.append(Caracter); }
                     break;
                 case 10:
-                    System.out.println("Caso 10");
+                    //System.out.println("Caso 10");
                     if (Character.isWhitespace(Caracter) /*u otro simbolo */) estado = 11;
-                    else if (Character.isDigit(Caracter)){ estado = 10; Palabra.append(Caracter); }
-                    else if (!Character.isDigit(Caracter)){ estado = 10; Palabra.append(Caracter); }
+                    else if (Character.isLetterOrDigit(Caracter)) Palabra.append(Caracter);
+                    else estado = 13;
                     break;
                 case 11:
-                    System.out.println("Caso 11");
+                    //System.out.println("Caso 11");
                     NuevoLexema = Palabra.toString();
+                    Palabra.setLength(0);
                     System.out.println(NuevoLexema);
-                    for (Map.Entry<String,TipoToken>entry:PalabrasReservadas.entrySet()){
-                        String palabrareservada = entry.getKey();
-                        if (NuevoLexema.equals(palabrareservada)) Tokens.add(new Token(PalabrasReservadas.get(NuevoLexema),NuevoLexema,null,linea));
-                    }
-                    Tokens.add(new Token(TipoToken.IDENTIFICADOR,NuevoLexema,null,linea));
+                    if (NuevoLexema.equals("y") | NuevoLexema.equals("clase") | NuevoLexema.equals("ademas") | NuevoLexema.equals("falso") | NuevoLexema.equals("por") |
+                        NuevoLexema.equals("fun") | NuevoLexema.equals("si") | NuevoLexema.equals("nulo") | NuevoLexema.equals("o") | NuevoLexema.equals("imprimir") |
+                        NuevoLexema.equals("retorno") | NuevoLexema.equals("super") | NuevoLexema.equals("este") | NuevoLexema.equals("verdadero") | NuevoLexema.equals("var") | NuevoLexema.equals("mientras"))
+                        for (Map.Entry<String,TipoToken>entry:PalabrasReservadas.entrySet()){
+                            String palabrareservada = entry.getKey();
+                            if (NuevoLexema.equals(palabrareservada)) Tokens.add(new Token(PalabrasReservadas.get(NuevoLexema),NuevoLexema,null,linea));
+                        }
+                    else Tokens.add(new Token(TipoToken.IDENTIFICADOR,NuevoLexema,null,linea));
                     estado = 9;
+                    i=retractar(i);
+                    //System.out.println(Tokens.get());
                     break;
                 default:
                     break;
             }
-            //System.out.println(Caracter);
+            System.out.println(Caracter + " Caso: " + estado);
         }
 
         /*
