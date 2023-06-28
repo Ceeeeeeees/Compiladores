f3f72435a32e52a74f376dedcac4a4adfd0a2790
CesarSM1 <cesarsmore@gmail.com>
2023-06-28 00:29:33 -0600

    Zz
    z

--- Interprete/Arbol.java
+++ Interprete/Arbol.java
@@ -5,14 +5,14 @@ import javax.crypto.spec.OAEPParameterSpec;
 public class Arbol {
     private final Nodo raiz;
 
-    public Arbol(Nodo raiz){
+    public Arbol(Nodo raiz) {
         this.raiz = raiz;
     }
 
-    public void recorrer(){
-        for(Nodo n : raiz.getHijos()){
+    public void recorrer() {
+        for (Nodo n : raiz.getHijos()) {
             Token t = n.getValue();
-            switch (t.tipo){
+            switch (t.tipo) {
                 // Operadores aritméticos
                 case SUMA:
                 case RESTA:
@@ -25,6 +25,7 @@ public class Arbol {
                 case Y:
                 case IDENTIFICADOR:
                 case O:
+<<<<<<< Updated upstream
                     Aritmetico solver = new Aritmetico(n,this.tablaSimbolos);
 >>>>>>> Stashed changes
                     Object res = solver.resolver();
@@ -32,13 +33,84 @@ public class Arbol {
                 break;
                 case VARIABLE:
                     // Crear una variable. Usar tabla de simbolos
+=======
+                    Aritmetico solver = new Aritmetico(n, this.tablaSimbolos);
+                    Object res = solver.resolver();
+                    System.out.println("Resultado de la operacion: " + res);
+                    break;
+                case VARIABLE:
+                    // Crear una variable. Usar tabla de simbolos
+                    Nodo identificador = n.getHijos().get(0);
+
+                    if (tablaSimbolos.ExisteIdentificador(identificador.getValue().lexema)) {
+                        System.out.println("Error: Variable Duplicada.");
+                        throw new RuntimeException("Variable Ya Definida: " + identificador.getValue().lexema);
+                    }
+
+                    if (n.getHijos().size() == 1) {
+                        tablaSimbolos.Asignar(identificador.getValue().lexema, null);
+                    } else {
+                        for (int i = 1; i < n.getHijos().size(); i++) {
+                            Nodo valor = n.getHijos().get(i);
+                            if (valor.getValue().tipo == TipoToken.NUMERO) {
+                                tablaSimbolos.Asignar(identificador.getValue().lexema, valor.getValue().literal);
+                            } else if (valor.getValue().tipo == TipoToken.CADENA) {
+                                tablaSimbolos.Asignar(identificador.getValue().lexema, valor.getValue().lexema);
+                            } else if (valor.getValue().tipo == TipoToken.IDENTIFICADOR) {
+                                tablaSimbolos.Asignar(identificador.getValue().lexema, tablaSimbolos.ObtenerValor(valor.getValue().lexema));
+                            } else if (valor.getValue().tipo == TipoToken.VERDADERO || valor.getValue().tipo == TipoToken.FALSO) {
+                                tablaSimbolos.Asignar(identificador.getValue().lexema, valor.getValue().lexema);
+                            } else if (valor.getValue().tipo == TipoToken.SUMA || valor.getValue().tipo == TipoToken.RESTA || valor.getValue().tipo == TipoToken.MULTIPLICACION || valor.getValue().tipo == TipoToken.DIVICION || valor.getValue().tipo == TipoToken.OPREL || valor.getValue().tipo == TipoToken.Y || valor.getValue().tipo == TipoToken.O) {
+                                Aritmetico solverVariable = new Aritmetico(valor, this.tablaSimbolos);
+                                Object resultadoVariable = solverVariable.resolver();
+                                tablaSimbolos.Asignar(identificador.getValue().lexema, resultadoVariable);
+                            } else {
+                                throw new RuntimeException("Tipo de dato no válido: " + valor.getValue().lexema);
+                            }
+                        }
+                    }
+>>>>>>> Stashed changes
                     break;
                 case SI:
 
+<<<<<<< Updated upstream
                 case SINO:
 <<<<<<< Updated upstream
 
                 case MIENTRAS:
+=======
+                    if (!(resultadoSi instanceof Boolean)) {
+                        throw new RuntimeException("La condicion no es booleana: " + resultadoSi);
+                    }
+
+                    Condicion = (Boolean) resultadoSi;
+                    if (Condicion) {
+                        Nodo bloque = n.getHijos().get(1);
+                        switch (bloque.getValue().tipo) {
+                            case IMPRIMIR:
+                                for (Nodo hijo : bloque.getHijos()) {
+                                    Aritmetico solverImprimir = new Aritmetico(hijo, this.tablaSimbolos);
+                                    Object resultado = solverImprimir.resolver();
+                                    System.out.println("Resultado del imprimir: " + resultado);
+                                }
+                                break;
+                        }
+                    } else {
+                        Nodo bloque = n.getHijos().get(2);
+                        for (Nodo hijo : bloque.getHijos()) {
+                            switch (hijo.getValue().tipo) {
+                                case IMPRIMIR:
+                                    for (Nodo bijo : bloque.getHijos()) {
+                                        Nodo aux = bijo.getHijos().get(0);
+                                        Aritmetico solverImprimir = new Aritmetico(aux, this.tablaSimbolos);
+                                        Object resultado = solverImprimir.resolver();
+                                        System.out.println("Resultado del imprimir: " + resultado);
+                                    }
+                                    break;
+                            }
+                            break;
+                        }
+>>>>>>> Stashed changes
 
                 case POR:
                     EstructurasControl solve = new EstructurasControl(n);
@@ -48,6 +120,7 @@ public class Arbol {
 
 
                 case IMPRIMIR:
+<<<<<<< Updated upstream
                     break;
 =======
 
@@ -64,18 +137,118 @@ public class Arbol {
 
                 case IMPRIMIR:
                     for (Nodo bijo : n.getHijos()){
+=======
+                    for (Nodo bijo : n.getHijos()) {
+>>>>>>> Stashed changes
                         Aritmetico solverImprimir = new Aritmetico(bijo, this.tablaSimbolos);
                         Object resultado = solverImprimir.resolver();
                         System.out.println("Resultado del imprimir: " + resultado);
                     }
                     break;
 
+<<<<<<< Updated upstream
 
+>>>>>>> Stashed changes
+=======
+                case POR:
+                    // Inicialización
+                    Nodo inicializacion = n.getHijos().get(0);
+                    Nodo valorInicializacion = inicializacion.getHijos().get(0);
+                    tablaSimbolos.Asignar(inicializacion.getValue().lexema, valorInicializacion.getValue().literal);
+
+                    // Condición
+                    Nodo condicionPor = n.getHijos().get(1);
+                    Aritmetico solverCondicionPor = new Aritmetico(condicionPor, this.tablaSimbolos);
+                    Object resultadoCondicionPor = solverCondicionPor.resolver();
+
+                    if(!(resultadoCondicionPor instanceof Boolean)){
+                        throw new RuntimeException("La condicion no es booleana: " + resultadoCondicionPor);
+                    }
+
+                    // Incremento o decremento
+                    Nodo incremento = n.getHijos().get(2);
+                    Aritmetico solverIncremento = new Aritmetico(incremento, this.tablaSimbolos);
+
+                    // Bloque de código
+                    Nodo bloquePor = n.getHijos().get(3);
+
+                    while ((Boolean) resultadoCondicionPor){
+                        // Ejecutar el bloque de código
+                        for (Nodo hijo : bloquePor.getHijos()){
+                            switch (hijo.getValue().tipo){
+                                case IMPRIMIR:
+                                    for (Nodo bijo : hijo.getHijos()){
+                                        Aritmetico solverImprimir = new Aritmetico(bijo, this.tablaSimbolos);
+                                        Object resultado = solverImprimir.resolver();
+                                        System.out.println("Resultado del imprimir: " + resultado);
+                                    }
+                                    break;
+                                // Aquí puedes agregar más casos para otros tipos de instrucciones
+                            }
+                        }
+
+                        // Incrementar o decrementar la variable
+                        solverIncremento.resolver();
+
+                        // Evaluar de nuevo la condición
+                        resultadoCondicionPor = solverCondicionPor.resolver();
+                    }
+                    break;
+
+                case MIENTRAS:
+                    Nodo condicionMientras = n.getHijos().get(0);
+                    Object resultadoMientras;
+
+                    do {
+                        Aritmetico solverMientras = new Aritmetico(condicionMientras, this.tablaSimbolos);
+                        resultadoMientras = solverMientras.resolver();
+
+                        if (!(resultadoMientras instanceof Boolean)) {
+                            throw new RuntimeException("La condicion no es booleana: " + resultadoMientras);
+                        }
+
+                        if ((Boolean) resultadoMientras) {
+
+                            for (int i = 1 ; i < n.getHijos().size() ; i++) {
+                                switch (n.getValue().tipo) {
+                                    case IMPRIMIR:
+                                        for (Nodo mijo : n.getHijos()) {
+                                            Aritmetico solverImprimir = new Aritmetico(mijo, this.tablaSimbolos);
+                                            Object resultado = solverImprimir.resolver();
+                                            System.out.println("Resultado: " + resultado);
+                                        }
+                                        break;
+
+                                    case SUMA:
+                                    case RESTA:
+                                    case MULTIPLICACION:
+                                    case DIVICION:
+                                    case OPREL:
+                                    case Y:
+                                    case O:
+
+                                            Aritmetico solverMijo = new Aritmetico(n, this.tablaSimbolos);
+                                            Object resultado = solverMijo.resolver();
+                                            System.out.println("Resultado: " + resultado);
+
+                                        break;
+                                    // Aquí puedes agregar más casos para otros tipos de instrucciones
+                                }
+                            }
+                        }
+
+                        // Actualizar la condición del bucle después de que todas las instrucciones se hayan ejecutado
+                        resultadoMientras = solverMientras.resolver();
+                        Condicion = (Boolean) resultadoMientras;
+                    } while (Condicion);
+                    break;
 >>>>>>> Stashed changes
 
             }
         }
     }
+}
+
 
 }
 
